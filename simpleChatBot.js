67aa353ba3eeb3796e5898d6129933be38de4899
carlosdvp <carlosdvp@yahoo.com>
2018-03-10 20:07:54 -0400

    added event listener to the enter key

--- simpleChatBot.js
+++ simpleChatBot.js
@@ -17,5 +17,26 @@ var answer = document.querySelector('#user-input');
 botQuestion.innerHTML = questions[2];
 
 function showResponse() {
-
+	// var input = answer.value;
+	// if(answer.value )
+	alert('yeah!!!');
 }
+
+// we add the vent listener for the enter key
+
+// with jquery:
+// $(document).on('keypress', function(e) {
+// 	if (e.which == 13) {
+// 		showResponse();
+// 	} 
+// });
+
+// vanila JS w/the new specs
+document.addEventListener('keyup', function(event) {
+	var key = event.key || event.keyCode;
+	if (key === 'Enter' || key === 'enter' || key === 13) {
+		showResponse();
+	}
+});
+
+
